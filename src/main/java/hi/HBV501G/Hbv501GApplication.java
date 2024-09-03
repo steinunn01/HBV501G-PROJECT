745ab299e9dae896e70f9108750fe8511c6ff53b
steinunn01 <steinunnm01@gmail.com>
2024-09-03 18:04:36 +0000

    init

---
+++ src/main/java/hi/HBV501G/Hbv501GApplication.java
@@ -0,0 +1,13 @@
+package hi.HBV501G;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hbv501GApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hbv501GApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hi/HBV501G/Hbv501GApplicationTests.java
@@ -0,0 +1,13 @@
+package hi.HBV501G;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hbv501GApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
