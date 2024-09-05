fb73ec014e1843f7fee878f456726324b87b4dd3
brynjar <brynjar23@live.com>
2024-09-05 17:27:02 +0000

    added a comment

--- src/main/java/hi/HBV501G/MyController.java
+++ src/main/java/hi/HBV501G/MyController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
     @GetMapping("/api/greeting")
-    public String greeting(){
+    public String greeting() {
         return "Hello Team 20, this is one method in your API";
     }
 }
+// brynjar was here
