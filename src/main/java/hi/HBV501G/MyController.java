654a617289ae8ab0cd831887a4a29bf49b3a9227
Catarina (Cat) Lima <catisousalima1999@gmail.com>
2024-09-05 17:13:19 +0000

    Controller class and test html
    Making sure spring web dependency works.

---
+++ src/main/java/hi/HBV501G/MyController.java
@@ -0,0 +1,13 @@
+package hi.HBV501G;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/api/greeting")
+    public String greeting(){
+        return "Hello Team 20, this is one method in your API";
+    }
+}
